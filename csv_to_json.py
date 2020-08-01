67e6419dc6affdf07dcc967d3cac2d1e5bf0e372
cibilsmash <54404426+cibilsmash@users.noreply.github.com>
2020-08-02 00:38:31 +0530

    Add files via upload

--- csv_to_json.py
+++ csv_to_json.py
@@ -2,58 +2,37 @@ import csv
 
 import json
 
-
-csv_File = 'sample.csv'
 json_File = 'students.json'
 
-def make_json(csv_File, json_File):
-
-    data = {}
-    with open(csv_File, encoding='utf-8') as csv_f:
-        csv_data = csv.DictReader(csv_f)
+csv_File_path = 'sample.csv'
 
 
-
-        for rows in csv_data:
-
-            key = rows['Name']
-
-            if type(rows) is int:
+def View_json(csv_File_path):
 
 
-                data[key] = int(rows)
+    data4 = []
+    with open(csv_File_path, encoding='utf-8') as csv_fi:
+        csv_data1 = csv.reader(csv_fi)
 
+        data1 = list(csv_data1)
+
+        result = sum(data1, [])
+
+        for i in result:
+            if i == "":
+                data4.append("NULL")
             else:
-
-                data[key] = rows
-
-
-
+                data4.append(i)
 
     with open(json_File, 'w', encoding='utf-8') as json_f:
-        json_f.write(json.dumps(data, indent=4))
+                    json_f.write(json.dumps(data4))
 
-
-make_json(csv_File, json_File)
-
-
+View_json(csv_File_path)
 
 
 
-csv_file = 'sample.csv'
 
 
-merge = []
-
-def view_sample(csv_file):
-
-    with open(csv_file, encoding='utf-8') as csv_f:
-
-        csv_data = csv.reader(csv_f)
-
-        dict_list = list(csv_data)
-
-        print(dict_list)
 
 
 
